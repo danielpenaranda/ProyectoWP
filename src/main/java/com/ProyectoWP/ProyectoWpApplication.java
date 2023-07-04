eb60d990180863b41fd78e4af6bc1e0d2d44a2ef
Daniel Peñaranda <danielpe207@gmail.com>
2023-07-03 18:12:32 -0600

    Inicialización de proyecto de la tienda de videojuegos

---
+++ src/main/java/com/ProyectoWP/ProyectoWpApplication.java
@@ -0,0 +1,13 @@
+package com.ProyectoWP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoWpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoWpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ProyectoWP/ProyectoWpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ProyectoWP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoWpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
