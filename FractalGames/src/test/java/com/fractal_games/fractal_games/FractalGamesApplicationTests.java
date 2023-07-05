4f29c0dd2af011d60327c886d2fa4c6519f99fe6
Jgaray3008 <110505188+Jgaray3008@users.noreply.github.com>
2023-07-05 01:12:16 -0600

    nuevo directorio a utilizar base

---
+++ FractalGames/src/test/java/com/fractal_games/fractal_games/FractalGamesApplicationTests.java
@@ -0,0 +1,18 @@
+package com.fractal_games.fractal_games;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Configuration;
+
+@SpringBootTest
+class FractalGamesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Configuration
+    static class TestConfiguration {
+        // Configuración de las pruebas
+    }
+}
