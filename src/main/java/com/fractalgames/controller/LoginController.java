32aa038e23ad814d5b884e3a8caf3ee4e0405a3a
Jgaray3008 <moralesjoseph11@gmail.com>
2023-07-06 22:44:32 -0600

    se arreglo login, login lleva a inicio al iniciar sesion con un usuario valido

---
+++ src/main/java/com/fractalgames/controller/LoginController.java
@@ -0,0 +1,40 @@
+package com.fractalgames.controller;
+
+
+import com.fractalgames.domain.Usuario;
+import com.fractalgames.sevice.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+public class LoginController {
+
+    private final UsuarioService usuarioService;
+
+    @Autowired
+    public LoginController(UsuarioService usuarioService) {
+        this.usuarioService = usuarioService;
+    }
+
+    @GetMapping("/login/listado")
+    public String login(Model model) {
+        model.addAttribute("usuario", new Usuario());
+        return "login/listado";
+    }
+
+    @PostMapping("/login/guardar")
+    public String login(@ModelAttribute("usuario") Usuario usuario, RedirectAttributes attributes) {
+        if (usuarioService.validarCredenciales(usuario.getEmail(), usuario.getPassword())) {
+            attributes.addFlashAttribute("mensaje", "Inicio de sesión exitoso");
+            return "redirect:/index";
+        } else {
+            attributes.addFlashAttribute("error", "Correo electrónico o contraseña incorrectos");
+            return "redirect:/login/listado";
+        }
+    }
+}
