5e75194de669f169585ff31858478a7c3aaf8e8d
Jgaray3008 <moralesjoseph11@gmail.com>
2023-08-19 04:27:03 -0600

    Se arregla parte de login y register, se intenta agregar autenticacion, se actualizan partes de diversos archivos

--- src/main/java/com/fractalgames/controller/LoginController.java
+++ src/main/java/com/fractalgames/controller/LoginController.java
@@ -2,19 +2,13 @@ package com.fractalgames.controller;
 
 
 import com.fractalgames.domain.Usuario;
-import com.fractalgames.sevice.UsuarioDetailsService;
 import com.fractalgames.sevice.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
